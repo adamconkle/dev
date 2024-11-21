c147684ada5b7fe6e2fd3f19add95cf41cb0e92c
Adam <adam.conkle@gmail.com>
2024-11-21 12:45:05 -0800

    Update devscript.js

--- devscript.js
+++ devscript.js
@@ -1,1 +1,4 @@
+ <iframe src="me_portrait_circle.002.png" title="description"></iframe> 
+
+// Buy Me A Coffee
 <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="adamcdesigns" data-description="Support me on Buy me a coffee!" data-message="Your support is greatly appreciated!" data-color="#40DCA5" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
