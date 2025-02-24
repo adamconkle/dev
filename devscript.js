140ff166e77af1f068fd1a291739db5a25901ed0
Adam <adam.conkle@gmail.com>
2025-02-23 22:12:10 -0800

    revised iframe

--- devscript.js
+++ devscript.js
@@ -1,4 +1,13 @@
- <iframe src="me_portrait_circle.002.png" title="description"></iframe> 
+function openIframe() {
+     document.getElementById('iframeOverlay').classList.add('show');
+}
+
+function closeIframe(event) {
+     if (event.target.classList.contains('iframe-overlay') || event.target.classList.contains('close-btn')) {
+         document.getElementById('iframeOverlay').classList.remove('show');
+     }
+}
+
 
 // Buy Me A Coffee
 <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="adamcdesigns" data-description="Support me on Buy me a coffee!" data-message="Your support is greatly appreciated!" data-color="#40DCA5" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
